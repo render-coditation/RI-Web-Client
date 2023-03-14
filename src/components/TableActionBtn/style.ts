33e8d596a70bfc6db1d8661c2d8b3bb01cd388f2
Omkar Jadhav <omkar.jadhav@coditation.com>
2023-03-14 20:48:15 +0530

    [feat] added Table action btns

---
+++ src/components/TableActionBtn/style.ts
@@ -0,0 +1,8 @@
+import styled from 'styled-components'
+import { ROUNDED_CIRCLE } from '../../constants/style/borders'
+
+export const TableActionBtnContainer = styled.button`
+  outline: none;
+  border: none;
+  border-radius: ${ROUNDED_CIRCLE};
+`
